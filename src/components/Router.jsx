f2138c8e3a55764e7d082fe7c2185f1699f1d14a
ggchicote <gchicote@awg.la>
2022-07-17 20:12:27 -0300

    more animation examples added

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -7,12 +7,14 @@ import InOutCircle from '../screens/InOutCircleScreen'
 import WhatsUp from '../screens/WhatsUpScreen';
 import ThemeAnimation from '../screens/ThemeAnimationScreen'
 import ZoomMatters from '../screens/ZoomMattersScreen'
+import Instagraming from '../screens/Instagraming'
+import OnBoarding from '../screens/OnBoarding'
 
 const Drawer = createDrawerNavigator();
 
 const Router = () => {
   return (
-      <Drawer.Navigator initialRouteName="ZoomMatters">
+      <Drawer.Navigator initialRouteName="OnBoarding">
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name="Raw animation" component={NoLibraries} />
         <Drawer.Screen name="Opacity" component={Opacity} />
@@ -20,6 +22,8 @@ const Router = () => {
         <Drawer.Screen name="What's up?" component={WhatsUp} />
         <Drawer.Screen name="Theme animation" component={ThemeAnimation} />
         <Drawer.Screen name="Zoom matters(?)" component={ZoomMatters} />
+        <Drawer.Screen name='Instagraming' component={Instagraming} />
+        <Drawer.Screen name='Onboarding' component={OnBoarding} />
       </Drawer.Navigator>
   );
 }
